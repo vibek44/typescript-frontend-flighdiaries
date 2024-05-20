8e93674ff563697bb3bc1f424684ca90db8e038d
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-20 19:16:35 +0300

    adding types needed for the typing the fetched data

--- src/types.tsx
+++ src/types.tsx
@@ -1,5 +1,26 @@
-export interface Notes{
-  id:number;
-  content:string;
+
+export enum Weather { 
+  Sunny='sunny' ,
+  Rainy='rainy' ,
+  Cloudy='cloudy' ,
+  Windy= 'windy' ,
+  Stormy= 'stormy'
+}
+export enum Visibility {
+  Great='great' ,
+  Good='good' ,
+  Ok= 'ok' ,
+  Poor= 'poor'
 }
 
+export interface Diary{
+  id: number;
+  date: string;
+  weather: Weather;
+  visibility: Visibility;
+  comment?: string;
+}
+
+//export type NewDiaryEntry=Omit<Diary, 'id'>;
+
+//export type NonSensitiveDiaryEntry=Omit<Diary, 'comment'>;
