6390b4ce19b2923178802842cdacde4ec93ca6bc
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-21 20:47:10 +0300

    adding form and form handler, parsing the form object and creating newdiary

--- src/components/EntryForm.tsx
+++ src/components/EntryForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { createDiaries } from "../services/diaryService";
 
 const EntryForm=()=>{
   const [date,setDate]=useState('');
@@ -8,16 +9,19 @@ const EntryForm=()=>{
 
   const submitDiary=(event:React.SyntheticEvent)=>{
     event.preventDefault()
+    createDiaries( {date,visibility,weather,comment});
+    
 
   }
  return(
   <form onSubmit={submitDiary}>
-    date: <input type='date' value={date} onChange={(event) => setDate(event.target.value)} />
-    visibility:<input type="text" value={visibility} onChange={(event)=>setVisibility(event.target.value)}/>
-    weather: <input type="text" value={weather} onChange={(event)=>setWeather(event.target.value)}/>
-    comment: <input type="text" value={comment} onChange={(event)=>setComment(event.target.value)}/>
-  
+
+    date: <input type='date' value={date} onChange={(event) => setDate(event.target.value)} /><br/>
+    visibility:<input type="text" value={visibility} onChange={(event)=>setVisibility(event.target.value)}/><br/>
+    weather: <input type="text" value={weather} onChange={(event)=>setWeather(event.target.value)}/><br/>
+    comment: <input type="text" value={comment} onChange={(event)=>setComment(event.target.value)}/><br/>
     <button type='submit'>add</button>
+    
   </form>
  )
 }
