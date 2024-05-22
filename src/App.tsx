0cab21d206af9ba07e6a3e91f500fd53ef0736fc
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-22 19:26:34 +0300

    adding function addDiaries to add entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import { Diary } from './types'
-import { getAllDiaries } from './services/diaryService'
+import { Diary,NewDiary } from './types'
+import { getAllDiaries,createDiaries } from './services/diaryService'
 import DiaryList from './components/DiaryList'
 import EntryForm from './components/EntryForm'
 const App=()=>{
   //const [date, setDate] = useState('');
-  const [diaries, setDiaries] = useState<Diary>([]);
+  const [diaries, setDiaries] = useState<Diary[]>([]);
 
   useEffect(()=>{
      getAllDiaries().then(data=>setDiaries(data)
      )
 },[])
+
+ const addDiaries=(newDiary:NewDiary)=>{
+    createDiaries(newDiary).then(data=>setDiaries(diaries.concat(data)))
+ }
+
   return  (
     <div>
       <h2>Diary Entry</h2>
-      <EntryForm/>
-      <ul>
+      <EntryForm addDiaries={addDiaries}/>
+      <h2><i>Entries</i></h2>
       { diaries.map(diary=><DiaryList key={diary.id} diary={diary}/>)
-      }</ul>
-      
+      } 
     </div>
   )
 }
