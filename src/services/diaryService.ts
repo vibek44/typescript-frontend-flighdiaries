ae2a186cc927bb65fa0cf8e6f14bc58cb3387c84
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-22 19:34:23 +0300

    adding createDiaries function for post service needed for app

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 import { Diary } from "../types";
+const baseUrl='http://localhost:3001/api/diaries'
+import { NewDiary } from "../types";
 
 export const getAllDiaries=()=>{
-   return axios.get<Diary[]>('http://localhost:3001/api/diaries').then(res=>res.data)
+   return axios.get<Diary[]>(baseUrl).then(res=>res.data)
+}
+
+export const createDiaries=(diaryObject:NewDiary)=>{
+  return axios.post<Diary>(baseUrl,diaryObject).then(res=>res.data)
 }
 
