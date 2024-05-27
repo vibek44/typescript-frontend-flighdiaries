00833b20b6314bbba6dfd095740afc161f9c5237
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-27 20:36:16 +0300

    importing and converting interface Visibility and Weather to use in input radio to select data

--- src/components/EntryForm.tsx
+++ src/components/EntryForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 //import toNewDiary from "../utils/parseEntry";
 import { EntriesProps } from "../types";
+import { Visibility,Weather } from "../types";
 
 
 const EntryForm=({addDiaries}:EntriesProps)=>{
@@ -26,8 +27,34 @@ const EntryForm=({addDiaries}:EntriesProps)=>{
   <form onSubmit={submitDiary}>
 
     date: <input type='date' value={date} onChange={(event) => setDate(event.target.value)} /><br/>
-    visibility:<input type="text" value={visibility} onChange={(event)=>setVisibility(event.target.value)}/><br/>
-    weather: <input type="text" value={weather} onChange={(event)=>setWeather(event.target.value)}/><br/>
+    <div  className="visibility">
+      <label> Visibility: </label>
+      {
+        Object
+        .values(Visibility)
+        .map(item=>{
+        return(
+          < div key={item}>
+            <label >{item}</label>
+            <input type="radio" id={item} name="visibility" value={item } onChange={(e)=>setVisibility(e.target.value)} />
+          </div>
+        )})
+      }
+    </div>
+    <div className="weather">
+      <label>weather:</label>
+      {
+        Object
+        .values(Weather)
+        .map((item)=>{
+        return(
+          <div key={item}>
+            <label>{item}</label>
+            <input type="radio" id={item} name="weather" value={ item } onChange={(e)=>setWeather(e.target.value)} />
+          </div>
+        )})
+      }
+    </div>
     comment: <input type="text" value={comment} onChange={(event)=>setComment(event.target.value)}/><br/>
     <button type='submit'>add</button>
     
