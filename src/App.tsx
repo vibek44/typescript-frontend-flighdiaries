0a5c433163642295b563e06fb71a4b9ab89d542c
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-05-27 18:35:05 +0300

    adding  type narrowing logic to handle both data and error from post request of diary Entry

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { Diary,NewDiary } from './types'
-import { getAllDiaries,createDiaries } from './services/diaryService'
-import DiaryList from './components/DiaryList'
-import EntryForm from './components/EntryForm'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Diary,NewDiary } from './types';
+import { getAllDiaries,createDiaries } from './services/diaryService';
+import DiaryList from './components/DiaryList';
+import EntryForm from './components/EntryForm';
+
 const App=()=>{
-  //const [date, setDate] = useState('');
+  const [error, setError] = useState('');
   const [diaries, setDiaries] = useState<Diary[]>([]);
 
   useEffect(()=>{
      getAllDiaries().then(data=>setDiaries(data)
      )
-},[])
+  },[])
 
- const addDiaries=(newDiary:NewDiary)=>{
-    createDiaries(newDiary).then(data=>setDiaries(diaries.concat(data)))
- }
-
+  const addDiaries=async(newDiary:NewDiary)=>{
+   
+      const data=await createDiaries(newDiary)
+      if(data &&  typeof data==='object' && 'id' in data && 'date' in data && 'comment' in data && 'visibility' in data){
+        setDiaries(diaries.concat(data))
+      }
+   
+      if( data && typeof data === 'object' && 'message' in data  ){
+        setError(data.message)
+        setTimeout(()=>{
+          setError('');
+        },3000) 
+    }
+  
+}  
   return  (
     <div>
+      {error && <p className='error'>{error}</p>}
       <h2>Diary Entry</h2>
       <EntryForm addDiaries={addDiaries}/>
       <h2><i>Entries</i></h2>
